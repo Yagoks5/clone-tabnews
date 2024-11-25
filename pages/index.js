4fbfd6d67322b59827023902d951e16b7842c756
Yago <113360810+Yagoks5@users.noreply.github.com>
2024-11-25 21:25:17 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Olá, Yogo's website here. Nice to meet you. Greetings from Iceland. </h1>
+    return <h1>Olá, Yogo's website here. Nice to meet you. Greetings from Iceland. Don't worry, this website is cool. =D </h1>
 }
 
 export default Home;
