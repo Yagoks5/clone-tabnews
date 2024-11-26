cae5c36bbfb0b413a9b806f9d48286c5d753a78a
Yago <113360810+Yagoks5@users.noreply.github.com>
2024-11-26 20:02:58 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Olá, Yogo's website here. Nice to meet you. Greetings from Iceland. Don't worry, this website is cool. =D </h1>
+function Home() {
+  return (
+    <h1>
+      Olá, Yogo's website here. Nice to meet you. Greetings from Iceland. Don't
+      worry, this website is cool. =D{" "}
+    </h1>
+  );
 }
 
 export default Home;
